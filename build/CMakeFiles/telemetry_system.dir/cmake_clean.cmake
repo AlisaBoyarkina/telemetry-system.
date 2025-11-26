7b5d9c82009e99f18b18c458cbd05dda0d29781b
AlisaBoyarkina <alicaboarkina14@gmail.com>
2025-11-26 23:32:52 +0400

    Week 1: Basic infrastructure (CMake, Device, Logger)

---
+++ build/CMakeFiles/telemetry_system.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/telemetry_system.dir/src/Device/Device.cpp.o"
+  "CMakeFiles/telemetry_system.dir/src/Device/Device.cpp.o.d"
+  "CMakeFiles/telemetry_system.dir/src/Logger/Logger.cpp.o"
+  "CMakeFiles/telemetry_system.dir/src/Logger/Logger.cpp.o.d"
+  "CMakeFiles/telemetry_system.dir/src/main.cpp.o"
+  "CMakeFiles/telemetry_system.dir/src/main.cpp.o.d"
+  "bin/telemetry_system"
+  "bin/telemetry_system.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/telemetry_system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
